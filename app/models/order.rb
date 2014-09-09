0ab1ed0508690d68482c5ef157f520c6b6835547
pdomokos <domokospeter@gmail.com>
2014-09-09 14:58:49 +0200

    usermanagement added

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,9 @@
 class Order < ActiveRecord::Base
   belongs_to :product
+  belongs_to :user
   has_many :product_params
   validates :product, presence: true
+  validates :user, presence: true
 
   after_validation :create_product_specific_params
 
