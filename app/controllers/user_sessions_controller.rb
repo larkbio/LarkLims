42f310f605a2be21212527a5b2561e05adb89897
Balint Domokos <bdomokos74@gmail.com>
2014-09-11 16:05:39 +0200

    Introduce page style - dashboard page template and style - browser page template and style - login page and style

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -6,7 +6,7 @@ class UserSessionsController < ApplicationController
 
   def create
     if @user = login(params[:email], params[:password])
-      redirect_back_or_to(:pages_index, notice: 'Login successful')
+      redirect_back_or_to(controller: 'pages', action: 'dashboard', notice: 'Login successful')
     else
       flash.now[:alert] = 'Login failed'
       render action: 'new'
@@ -15,6 +15,6 @@ class UserSessionsController < ApplicationController
 
   def destroy
     logout
-    redirect_to(:pages_index, notice: 'Logged out!')
+    redirect_to(controller: 'pages', action: 'dashboard', notice: 'Logged out!')
   end
 end
