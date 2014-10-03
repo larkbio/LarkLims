7fb310bd3ed52fea8f02fa7ad8da7a47dc339a08
Balint Domokos <bdomokos74@gmail.com>
2014-10-03 12:14:59 +0200

    Dashboard infinite scrolling

--- app/assets/javascripts/dashboard.js.coffee
+++ app/assets/javascripts/dashboard.js.coffee
@@ -2,8 +2,10 @@
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
 
+@activity_limit = 20
+
 @load_activities = ()  ->
-  $.ajax '/activities/index',
+  $.ajax '/activities/index?limit='+@activity_limit,
     type: 'GET'
     dataType: 'json'
     error: (jqXHR, textStatus, errorThrown) ->
@@ -13,6 +15,7 @@
 
       i = 0
       $("div#activity-list").empty()
+
       for act in data
         newactivity = $("#activity_template").children().first().clone()
         newid =  "act-" + i
@@ -27,4 +30,70 @@
         $("#"+newid+" div div.activity-title").html(act['activity_title'])
         $("#"+newid+" div div.activity-details span").html(act['activity_detail'])
         i += 1
+        before_date = act['date']
+        before_order_id = act['order_id']
+#        console.log "setting before_date: "+before_date
+      $("input#before_date")[0].value = before_date
+      $("input#before_order_id")[0].value = before_order_id
 
+@load_and_append_activities = () ->
+  before_date = $("input#before_date")[0].value
+  before_order_id = $("input#before_order_id")[0].value
+  $.ajax '/activities/index?limit='+@activity_limit+"&before_date="+before_date+"&before_order_id="+before_order_id,
+    type: 'GET'
+    dataType: 'json'
+    error: (jqXHR, textStatus, errorThrown) ->
+      console.log "AJAX Error: #{textStatus}"
+    success: (data, textStatus, jqXHR) ->
+      console.log "Successful AJAX call"
+      console.log(data)
+      i = 0
+
+      for act in data
+        newactivity = $("#activity_template").children().first().clone()
+        newid =  "act-" + i
+        newactivity.attr('id', newid)
+
+        newactivity.insertBefore($("#activity-list div#progress-sign"))
+
+        $("#"+newid+" i").addClass(act['activity_icon'])
+        $("#"+newid+" div div.activity-time span").html(act['timediff']+' ago')
+        $("#"+newid+" div div.activity-title").html(act['activity_title'])
+        $("#"+newid+" div div.activity-details span").html(act['activity_detail'])
+        i += 1
+        before_date = act['date']
+        before_order_id = act['order_id']
+
+      console.log "before_date = "+before_date+" before_id = "+before_order_id
+      $("input#before_date")[0].value = before_date
+      $("input#before_order_id")[0].value = before_order_id
+      $("#activity-list div#progress-sign").remove()
+
+@should_scroll = (elem) ->
+  doc_top = $(window).scrollTop()
+  doc_bottom = doc_top + $(window).height()
+  elem_top = $(elem).offset().top
+  elem_bottom = elem_top + $(elem).height()
+  return (elem_bottom <= doc_bottom) && (elem_top >= doc_top)
+
+scroll_handler = (event) ->
+  scroll_top = $(window).scrollTop()
+  scroll_down = false
+  if scroll_top > @last_top
+    scroll_down = true
+  @last_top = scroll_top
+
+  if scroll_down && should_scroll("#activity-list div.activity:last")
+    console.log "need scroll"
+    console.log event
+    $("#activity-list div.progress-sign").remove()
+    $("<div id=\"progress-sign\" class=\"activity progress-sign\"><div class=\"body\"><i class=\"fa fa-spinner fa-spin fa-stack-2x\"></i></div></div>").insertAfter($("#activity-list div.activity:last"))
+    load_and_append_activities()
+
+
+@dashboard_loaded = () ->
+  console.log "dashboard loading"
+  load_activities()
+  @last_top = 0
+  $(document).scroll (event) ->
+    scroll_handler(event)
--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,9 +1,64 @@
 class ActivitiesController < ApplicationController
   include ActionView::Helpers::DateHelper
   def index
-    activities =  []
+    lim = params[:limit].to_i
+    before_date = params[:before_date]
+    after_order_id = params[:before_order_id]
 
-    Order.all.each do |o|
+    create_activities = get_create_activities(lim, before_date, after_order_id)
+    arrive_activities = get_arrive_activities(lim, before_date, after_order_id)
+
+    activities = create_activities + arrive_activities
+
+    # descending by date, ascending by order_id
+    @activity_arr = activities.sort { |a,b| (a[:date].to_i == b[:date].to_i) ? ( a[:order_id] <=> b[:order_id] ) : ( b[:date].to_i <=> a[:date].to_i ) }
+
+    if limit_ok?(lim)
+      @activity_arr = @activity_arr[0..lim]
+    end
+
+    respond_to do |format|
+      format.html { render :index }
+      format.json {render json: @activity_arr}
+    end
+
+  end
+
+  private
+
+  def limit_ok?(limit)
+    return (limit>0 and limit<1000)
+  end
+
+  def get_date(date_str)
+    if date_str
+      return DateTime.parse(date_str)
+    else
+      return nil
+    end
+  end
+
+  def get_create_activities(lim, before_date, after_order_id)
+    if before_date
+      if after_order_id
+        before1 = DateTime.parse(before_date)-1.second
+        before2 = DateTime.parse(before_date)+1.second
+        orders_query = Order.where("( (order_date between \"#{before1.to_s}\"  and \"#{before2.to_s}\" ) and id < #{after_order_id} ) or ( order_date <  \"#{before_date}\" )")
+      else
+        orders_query = Order.where("order_date <  \"#{before_date}\"")
+      end
+    else
+      orders_query = Order.all
+    end
+
+    if limit_ok?(lim)
+      orders_query = orders_query.order('order_date DESC, id ASC').limit(lim)
+    else
+      orders_query = orders_query.order('order_date DESC, id ASC')
+    end
+
+    result = []
+    orders_query.each do |o|
       usr = user_path(o.user)
       opath = order_url(o)
       url_short = o.url
@@ -12,23 +67,44 @@ class ActivitiesController < ApplicationController
       end
       title = "<a href=\"#{usr}\">#{o.user.name}</a>\n<span>added order</span>\n<a href=\"#{opath}\">#{o.comment}</a> (#{o.product.name})\n"
       detail = "From: #{o.ordered_from} <a href=\"#{o.url}\">#{url_short}</a>"
-      activities.append({:date => o.order_date ,:timediff => time_ago_in_words( o.order_date),
-                         :activity_title => title, :activity_detail => detail, :activity_icon => "fa-plus"})
+      result.append({:date => o.order_date ,:timediff => time_ago_in_words( o.order_date),
+                         :activity_title => title, :activity_detail => detail,
+                         :activity_icon => "fa-plus", :order_id => o.id,
+                         :status => 0})
     end
-
-    Order.where("status = 1").each do |o|
-        detail = "Cat: #{o.catalog_number}, Place: #{o.place}"
-        opath = order_url(o)
-        title = "<span>order</span>\n<a href=\"#{opath}\">#{o.comment}</a> (#{o.product.name})\n<span>arrived</span>\n"
-        activities.append({:date => o.arrival_date ,:timediff => time_ago_in_words( o.arrival_date),
-                       :activity_title => title, :activity_detail => detail, :activity_icon => "fa-check"})
-    end
-
-
-    a_sort = activities.sort_by { |item| item[:date].to_i }
-    a_sort_rev = a_sort.reverse
-
-    a_sort_rev.each {|i| puts "#{i[:date]} - #{i[:timediff]}"}
-    render json: a_sort_rev
+    return(result)
   end
+
+  def get_arrive_activities(lim, before_date, after_order_id)
+    if before_date
+      before1 = DateTime.parse(before_date)-1.second
+      before2 = DateTime.parse(before_date)+1.second
+      if after_order_id
+        completed_query = Order.where("( (arrival_date between \"#{before1.to_s}\" and \"#{before2.to_s}\" ) and id < #{after_order_id} ) or ( arrival_date <  \"#{before_date}\" )")
+      else
+        completed_query = Order.where("arrival_date <  \"#{before_date}\"")
+      end
+    else
+      completed_query = Order.all
+    end
+
+    completed_query = completed_query.where("status = 1")
+    if limit_ok?(lim)
+      completed_query = completed_query.order('arrival_date DESC, id ASC').limit(lim)
+    else
+      completed_query = completed_query.order('arrival_date DESC, id ASC').all
+    end
+    result = []
+    completed_query.each do |o|
+      detail = "Cat: #{o.catalog_number}, Place: #{o.place}"
+      opath = order_url(o)
+      title = "<span>order</span>\n<a href=\"#{opath}\">#{o.comment}</a> (#{o.product.name})\n<span>arrived</span>\n"
+      result.append({:date => o.arrival_date ,:timediff => time_ago_in_words( o.arrival_date),
+                         :activity_title => title, :activity_detail => detail,
+                         :activity_icon => "fa-check", :order_id => o.id,
+                         :status => 1})
+    end
+    return(result)
+  end
+
 end
